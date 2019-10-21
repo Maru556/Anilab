8014541b1d8449112186e280d3abbbdcf8cea69f
Dave <ophustle@gmail.com>
2019-10-21 17:41:28 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,5 @@
 export const environment = {
   production: true,
-  firebase: {
-    apiKey: 'AIzaSyCBmBF7WemiaGz9FBhLcIzBauciZbYx2JE',
-    authDomain: 'anilab-6be14.firebaseapp.com',
-    databaseURL: 'https://anilab-6be14.firebaseio.com',
-    projectId: 'anilab-6be14',
-    storageBucket: 'anilab-6be14.appspot.com',
-    messagingSenderId: '257358841663',
-    appId: '1:257358841663:web:8021c3b0b50462df60f58a'
+
   }
 };
