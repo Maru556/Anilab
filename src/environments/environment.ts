ff6b046067613ad31f5f9662f98ca5c12194bfbd
Dave <ophustle@gmail.com>
2019-10-22 10:06:08 +0200

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,12 @@
 export const environment = {
   production: false,
+    firebase: {
+    apiKey: 'XXXXXXXXXXXXXXXXX',
+    authDomain: 'XXXXXXXXXXXXXXXXX',
+    databaseURL: 'XXXXXXXXXXXXXXXXX',
+    projectId: 'XXXXXXXXXXXXXXXXX',
+    storageBucket: 'XXXXXXXXXXXXXXXXX',
+    messagingSenderId: 'XXXXXXXXXXXXXXXXX',
+    appId: 'XXXXXXXXXXXXXXXXX'
+  }
 };
