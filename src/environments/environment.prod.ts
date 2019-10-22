ed96ef844ee96236cc1eea6d833f788b0fc05c7e
Dave <ophustle@gmail.com>
2019-10-22 10:05:50 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,13 @@
 export const environment = {
   production: true,
-
+  firebase: {
+    apiKey: 'XXXXXXXXXXXXXXXXX',
+    authDomain: 'XXXXXXXXXXXXXXXXX',
+    databaseURL: 'XXXXXXXXXXXXXXXXX',
+    projectId: 'XXXXXXXXXXXXXXXXX',
+    storageBucket: 'XXXXXXXXXXXXXXXXX',
+    messagingSenderId: 'XXXXXXXXXXXXXXXXX',
+    appId: 'XXXXXXXXXXXXXXXXX'
+  }
   }
 };
